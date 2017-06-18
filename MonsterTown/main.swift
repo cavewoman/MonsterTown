b4fac1360d002719574b6a617c27c2e710e4730d
Anna Chan <anna@firma8.com>
2017-06-17 20:12:35 -0400

    Initial Commit

---
+++ MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Anna Chan on 6/17/17.
+//  Copyright © 2017 Anna Chan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
