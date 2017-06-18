28a0fbf84bf726e7b73c3ab887958030c25d6da0
Anna Chan <anna@firma8.com>
2017-06-18 10:42:13 -0400

    Makes the getter internal and the setter private for isFallingApart for Zombie

--- MonsterTown/Zombie.swift
+++ MonsterTown/Zombie.swift
@@ -13,7 +13,7 @@ class Zombie: Monster {
         return "Brains..."
     }
     var walksWithLimp = true
-    private var isFallingApart = false
+    internal private(set) var isFallingApart = false
     
     final override func terrorizeTown() {
         if !isFallingApart {
